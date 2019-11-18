908c8b22da3a697b19a35ecf3cbb6ab45cd8b0e6
Ruslan Ruhalov <r.rugalov@gmail.com>
2019-11-18 17:05:09 +0200

    feat(ui): Added expanded content parsing.

--- src/components/certificates-viewer/certificates-viewer.tsx
+++ src/components/certificates-viewer/certificates-viewer.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Prop, State } from '@stencil/core';
 import { Certificate } from '../../utils/crypto';
+import dayjs from 'dayjs';
 
 @Component({
   tag: 'pv-certificates-viewer',
@@ -41,12 +42,48 @@ export class CertificatesViewer {
       : signature;
   }
 
+  renderDN(item: Certificate['subject'] | Certificate['issuer']) {
+    return Object.keys(item).map(subject => {
+      return (
+        <p>
+          <span>{item[subject].name}</span>
+          <span>{item[subject].value}</span>
+        </p>
+      )
+    })
+  }
+
+  renderMetaData(item: Certificate) {
+    return ([
+      <p>
+        <span>Serial number</span>
+        <span>{item.serialNumber}</span>
+      </p>,
+      <p>
+        <span>Version</span>
+        <span>{item.version}</span>
+      </p>,
+      <p>
+        <span>Validity</span>
+        <span>{item.validity} days</span>
+      </p>,
+      <p>
+        <span>Issued</span>
+        <span>{dayjs(item.notBefore).format('ddd, MMM D, YYYY h:mm:ss')}</span>
+      </p>,
+      <p>
+        <span>Expired</span>
+        <span>{dayjs(item.notAfter).format('ddd, MMM D, YYYY h:mm:ss')}</span>
+      </p>,
+    ])
+  }
+
   renderCertificates() {
 
     return this.certificatesDecoded.map(certificate => {
       const isExpanded = certificate.signature.value === this.expanded;
 
-      return (
+      return ([
         <tr class={isExpanded && 'fill_grey'}>
           <td>
             {certificate.commonName}
@@ -60,8 +97,21 @@ export class CertificatesViewer {
             <button onClick={certificate.downloadAsDER}>Download DER</button>
           </td>
           <td />
-        </tr>
-    )})
+        </tr>,
+        isExpanded && (
+          <tr class="fill_grey">
+            <td colSpan={1}>
+              {this.renderDN(certificate.subject)}
+            </td>
+            <td colSpan={1}>
+              {this.renderDN(certificate.issuer)}
+            </td>
+            <td colSpan={2}>
+              {this.renderMetaData(certificate)}
+            </td>
+          </tr>
+        )
+    ])})
   }
 
   render() {
