85feb678df60bd994f4cfcb6770ef2d3df0a9588
donskov <donskovdmitriyv@gmail.com>
2019-11-27 17:47:54 +0200

    Updates

--- build/pv-certificates-viewer.esm.js
+++ build/pv-certificates-viewer.esm.js
@@ -1,1 +1,6 @@
-import{p as e,b as t}from"./p-a9d44150.js";e().then(e=>t([["p-skytrbk4",[[1,"pv-button",{fill:[1],disabled:[4],href:[1],target:[1]}]]],["p-ihafbbnu",[[1,"pv-text-hider",{opened:[1540]},[[0,"textExpand","textExpandHandler"]]],[1,"pv-certificate-summary",{certificate:[16],showIssuer:[4,"show-issuer"]}]]],["p-vg9i5x5k",[[1,"pv-certificate-viewer",{certificate:[1]}],[1,"pv-button-split",{onClick:[16],fill:[1],actions:[16],activeSplit:[32]}]]],["p-3srhkmh2",[[1,"pv-certificates-viewer",{certificates:[16],certificatesDecoded:[32],expandedRow:[32],certificateSelectedForDetails:[32],isDecodeInProcess:[32]}]]]],e));
+import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-facd9e82.js';
+
+patchBrowser().then(options => {
+  globals();
+  return bootstrapLazy([["pv-button-split",[[1,"pv-button-split",{"onClick":[16],"fill":[1],"actions":[16],"activeSplit":[32]}]]],["pv-text-hider",[[1,"pv-text-hider",{"opened":[1540]},[[0,"textExpand","textExpandHandler"]]]]],["pv-certificate-summary",[[1,"pv-certificate-summary",{"certificate":[16],"showIssuer":[4,"show-issuer"]}]]],["pv-button",[[1,"pv-button",{"fill":[1],"disabled":[4],"href":[1],"target":[1]}]]],["pv-certificate-viewer",[[1,"pv-certificate-viewer",{"certificate":[1]}]]],["pv-certificates-viewer",[[1,"pv-certificates-viewer",{"certificates":[16],"certificatesDecoded":[32],"expandedRow":[32],"certificateSelectedForDetails":[32],"isDecodeInProcess":[32]}]]]], options);
+});
