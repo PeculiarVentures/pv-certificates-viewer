b821b27e8171248200810f904556762fed109fe9
donskov <donskovdmitriyv@gmail.com>
2019-12-04 14:49:42 +0200

    chore(certificate): Updated decode version  key

--- src/utils/crypto/Certificate.ts
+++ src/utils/crypto/Certificate.ts
@@ -451,8 +451,15 @@ export default class Certificate extends Basic {
         )
       : undefined;
 
-    // decode version
-    this.version = pkijsSchema.version;
+    /**
+     * Decode version
+     *
+     * for value 2 - version 3
+     * for value 1 - version 2
+     *
+     * https://tools.ietf.org/html/rfc5280#section-4.1.2.1
+     */
+    this.version = pkijsSchema.version + 1;
 
     if (fullDecode) {
       // decode extensions
