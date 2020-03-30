fc84a18afe00d5dd55270eb78321459f999fa4b4
donskov <donskovdmitriyv@gmail.com>
2020-03-30 18:12:28 +0300

    fix(lint): Fixed problems

--- src/components/certificate-decoder/certificate-decoder.e2e.ts
+++ src/components/certificate-decoder/certificate-decoder.e2e.ts
@@ -3,9 +3,11 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('pv-certificate-decoder', () => {
   it('renders', async () => {
     const page = await newE2EPage();
+
     await page.setContent('<pv-certificate-decoder></pv-certificate-decoder>');
 
     const element = await page.find('pv-certificate-decoder');
+
     expect(element).toHaveClass('hydrated');
   });
 });
