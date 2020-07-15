dacf7ff2e0e69d7791eba16bc595b75aecc3693a
donskov <donskovdmitriyv@gmail.com>
2020-07-15 14:27:05 +0300

    feat(viewer): Added "CRLReason" extension

---
+++ src/components/certificate-viewer/extensions/crl_reason.tsx
@@ -0,0 +1,16 @@
+import { CRLReason, CRLReasons } from '@peculiar/asn1-x509';
+
+import { rowValue } from '../row_value';
+import { Extension } from '../../../crypto/extension';
+
+import { basic } from './basic';
+
+export function crlReason(extension: Extension, value: CRLReason) {
+  return basic(
+    extension,
+    rowValue(
+      'Reason',
+      CRLReasons[value.reason],
+    ),
+  );
+}
