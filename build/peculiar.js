7315372f04b8d1a450cca383dfcce3abe5408e09
donskov <donskovdmitriyv@gmail.com>
2020-07-16 11:42:57 +0300

    Updates

--- build/peculiar.js
+++ build/peculiar.js
@@ -125,7 +125,7 @@ var StyleNode=function(){this.start=0,this.end=0,this.previous=null,this.parent=
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-43b3c497.system.js', new URL(resourcesUrl, window.location.origin));
+    var url = new URL('./p-0bb9d9b9.system.js', resourcesUrl);
     System.import(url.href);
   };
 
