8e6564a80d8774857fef8eb9fdf09c302771f1b1
donskov <donskovdmitriyv@gmail.com>
2020-08-27 13:09:22 +0300

    feat(webcomponents): added IssuerName component

---
+++ packages/webcomponents/src/components/certificate-viewer/issuer_name.tsx
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { h, FunctionalComponent } from '@stencil/core';
+
+import type { INameJSON } from '../../crypto/name';
+
+import { RowTitle, RowValue } from './row';
+
+interface ISubjectNameProps {
+  name: INameJSON[];
+  issuerDnLink?: string;
+}
+
+export const IssuerName: FunctionalComponent<ISubjectNameProps> = (props) => {
+  const { name, issuerDnLink } = props;
+
+  if (!name) {
+    return null;
+  }
+
+  const title = 'Issuer Name';
+
+  return [
+    <RowTitle
+      value={issuerDnLink ? (
+        <peculiar-link
+          href={issuerDnLink}
+          type="h6"
+        >
+          {title}
+        </peculiar-link>
+      ) : title}
+    />,
+    name.map((n) => (
+      <RowValue
+        name={n.name || n.type}
+        value={n.value}
+      />
+    )),
+  ];
+};
