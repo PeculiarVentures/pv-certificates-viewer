d0d91a627c0409960fd844eec06320a1c674950b
donskov <donskovdmitriyv@gmail.com>
2020-08-27 13:09:34 +0300

    feat(webcomponents): added SubjectName component

---
+++ packages/webcomponents/src/components/certificate-viewer/subject_name.tsx
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { h, FunctionalComponent } from '@stencil/core';
+
+import type { INameJSON } from '../../crypto/name';
+
+import { RowTitle, RowValue } from './row';
+
+interface ISubjectNameProps {
+  name: INameJSON[];
+}
+
+export const SubjectName: FunctionalComponent<ISubjectNameProps> = (props) => {
+  const { name } = props;
+
+  if (!name) {
+    return null;
+  }
+
+  return [
+    <RowTitle
+      value="Subject Name"
+    />,
+    name.map((n) => (
+      <RowValue
+        name={n.name || n.type}
+        value={n.value}
+      />
+    )),
+  ];
+};
