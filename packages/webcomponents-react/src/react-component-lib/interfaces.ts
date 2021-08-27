a25df2e3aec8e610d38d887fb4c1e6538ab7eedd
donskov <donskovdmitriyv@gmail.com>
2021-08-27 11:45:05 +0300

    chore(webcomponents-react): update utils using new stencil-react dependency

--- packages/webcomponents-react/src/react-component-lib/interfaces.ts
+++ packages/webcomponents-react/src/react-component-lib/interfaces.ts
@@ -1,3 +1,8 @@
+// General types important to applications using stencil built components
+export interface EventEmitter<T = any> {
+  emit: (data?: T) => CustomEvent<T>;
+}
+
 export interface StyleReactProps {
   class?: string;
   className?: string;
