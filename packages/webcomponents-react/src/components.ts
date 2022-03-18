1bb60e232eb558acd86c15cad7a1f2a6c1e69e5a
donskov <donskovdmitriyv@gmail.com>
2022-03-18 15:41:54 +0200

    chore(webcomponents-react): add PeculiarCsrViewer and PeculiarAttributeCertificateViewer exports

--- packages/webcomponents-react/src/components.ts
+++ packages/webcomponents-react/src/components.ts
@@ -13,5 +13,7 @@ import { createReactComponent } from './react-component-lib';
 import { JSX } from '@peculiar/certificates-viewer';
 
 export const PeculiarCertificateViewer = /*@__PURE__*/createReactComponent<JSX.PeculiarCertificateViewer, HTMLPeculiarCertificateViewerElement>('peculiar-certificate-viewer');
+export const PeculiarCsrViewer = /*@__PURE__*/createReactComponent<JSX.PeculiarCsrViewer, HTMLPeculiarCsrViewerElement>('peculiar-csr-viewer');
+export const PeculiarAttributeCertificateViewer = /*@__PURE__*/createReactComponent<JSX.PeculiarAttributeCertificateViewer, HTMLPeculiarAttributeCertificateViewerElement>('peculiar-attribute-certificate-viewer');
 export const PeculiarCertificatesViewer = /*@__PURE__*/createReactComponent<JSX.PeculiarCertificatesViewer, HTMLPeculiarCertificatesViewerElement>('peculiar-certificates-viewer');
 export const PeculiarCertificateDecoder = /*@__PURE__*/createReactComponent<JSX.PeculiarCertificateDecoder, HTMLPeculiarCertificateDecoderElement>('peculiar-certificate-decoder');
