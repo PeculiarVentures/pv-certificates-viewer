1372eede5f5830e926362c2352286e85f532bca7
donskov <donskovdmitriyv@gmail.com>
2022-06-22 10:33:53 +0000

    Updates

---
+++ build/index.esm.js
@@ -0,0 +1,4 @@
+/*!
+ * © Peculiar Ventures https://peculiarventures.com/ - MIT License
+ */
+export{D as Download,d as downloadFromBuffer}from"./p-c774ec0b.js";import"./p-d0b55fbb.js";
---
+++ build/p-ee0e2707.system.js
@@ -0,0 +1,4 @@
+/*!
+ * © Peculiar Ventures https://peculiarventures.com/ - MIT License
+ */
+System.register(["./p-29cdd256.system.js","./p-88fbaf05.system.js"],(function(t){"use strict";return{setters:[function(e){var s={};s.Download=e.D;s.downloadFromBuffer=e.d;t(s)},function(){}],execute:function(){}}}));
