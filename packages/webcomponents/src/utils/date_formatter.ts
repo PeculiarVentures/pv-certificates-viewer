e940a2b57e78bddc787ed568ac2f8df5f3c1e7ca
Dmitriy Donskov <donskovdmitriyv@gmail.com>
2022-07-29 14:55:02 +0300

    chore(webcomponents): enhance validity duration print for granular durations (#118)

--- packages/webcomponents/src/utils/date_formatter.ts
+++ packages/webcomponents/src/utils/date_formatter.ts
@@ -7,6 +7,9 @@
  */
 
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
 
 export const dateShort = (date: string | number | Date) => (
   new Date(date).toUTCString()
@@ -22,21 +25,6 @@ export const dateDiff = (
 
   const start = dayjs(dateStart);
   const end = dayjs(dateEnd);
-  const ending = (value: number) => (value === 1 ? '' : 's');
 
-  const diffYears = end.diff(start, 'year', true);
-
-  if (Number.isInteger(diffYears)) {
-    return `${diffYears} year${ending(diffYears)}`;
-  }
-
-  const diffMonth = end.diff(start, 'month', true);
-
-  if (Number.isInteger(diffMonth)) {
-    return `${diffMonth} month${ending(diffMonth)}`;
-  }
-
-  const diffDays = end.diff(start, 'day');
-
-  return `${diffDays} day${ending(diffDays)}`;
+  return start.to(end, true);
 };
