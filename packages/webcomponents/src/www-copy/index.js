e513b49d007c0dcd3d913e2688f67ae79f0959e4
donskov <donskovdmitriyv@gmail.com>
2022-09-27 17:22:52 +0300

    feat(docs): add logic to work with "cert" and "certurl" query variables

---
+++ packages/webcomponents/src/www-copy/index.js
@@ -0,0 +1,80 @@
+(() => {
+  // Constants.
+  const CERTIFICATE_EXAMPLE = `-----BEGIN CERTIFICATE-----
+  MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+  TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+  cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+  WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+  ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+  MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+  h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+  0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+  A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+  T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+  B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+  B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+  KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+  OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+  jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+  qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+  rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+  HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+  hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+  ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+  3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+  NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+  ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+  TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+  jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+  oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+  4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+  mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+  emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+  -----END CERTIFICATE-----`;
+  const CERTIFICATE_DECODER_ELEMENT = document.createElement('peculiar-certificate-decoder');
+  const HISTORY = window.HistoryLibrary.createBrowserHistory();
+  const searchParams = new URLSearchParams(window.location.search);
+
+  // Listeners.
+  CERTIFICATE_DECODER_ELEMENT.addEventListener('successParse', (event) => {
+    HISTORY.replace(`?cert=${encodeURIComponent(event.detail)}`);
+
+    setTimeout(() => {
+      window.scrollTo({
+        top: 460,
+        behavior: 'smooth',
+      });
+    }, 50);
+  });
+  CERTIFICATE_DECODER_ELEMENT.addEventListener('clearCertificate', () => {
+    HISTORY.replace('/');
+  });
+
+  // Default props.
+  CERTIFICATE_DECODER_ELEMENT.certificateExample = CERTIFICATE_EXAMPLE;
+
+  if (searchParams.get('certurl')) {
+    window.fetch(searchParams.get('certurl'))
+      .then((body) => {
+        if (body.status >= 200 && body.status < 300) {
+          return body.text();
+        }
+
+        return Promise.reject();
+      })
+      .then((response) => {
+        CERTIFICATE_DECODER_ELEMENT.defaultCertificate = response;
+      })
+      .catch(() => {
+        alert('Failed to load certificate. Please use another file or check CORS policy.');
+      })
+      .finally(() => {
+        // Init.
+        content.appendChild(CERTIFICATE_DECODER_ELEMENT);
+      });
+  } else {
+    CERTIFICATE_DECODER_ELEMENT.defaultCertificate = searchParams.get('cert') || undefined;
+    // Init.
+    content.appendChild(CERTIFICATE_DECODER_ELEMENT);
+  }
+})();
