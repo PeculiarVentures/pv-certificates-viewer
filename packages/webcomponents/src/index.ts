d49a4f5ab48f6df5d7700ce64d449ee8a26749be
donskov <donskovdmitriyv@gmail.com>
2023-10-03 13:14:02 +0300

    chore(webcomponents): add function components export

--- packages/webcomponents/src/index.ts
+++ packages/webcomponents/src/index.ts
@@ -1,2 +1,7 @@
 export { downloadFromBuffer } from './utils/download_from_buffer';
 export { Download } from './utils/download';
+
+export { Typography } from './components/typography';
+export { Button } from './components/button';
+export { Link } from './components/link';
+export * from './components/icons';
