9d050a8c3fe27da43ad8b499093da3953e144ad4
donskov <donskovdmitriyv@gmail.com>
2024-05-10 12:10:47 +0300

    chore(webcomponents): add more utils export

--- packages/webcomponents/src/index.ts
+++ packages/webcomponents/src/index.ts
@@ -1,6 +1,12 @@
 export { downloadFromBuffer } from './utils/download_from_buffer';
 export { Download } from './utils/download';
-export * from './crypto';
+export {
+  readAsArrayBuffer,
+  readAsBinaryString,
+  readAsDataUrl,
+  readAsText,
+} from './utils/read_file';
+export { Pkcs10CertificateRequest } from './crypto';
 
 export { Typography } from './components/typography';
 export { Button } from './components/button';
