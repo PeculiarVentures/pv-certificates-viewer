bfdf5e8a92430d03165192db77c9f123b8a77021
Donskov Moodys <163475606+donskov-m@users.noreply.github.com>
2025-05-20 12:41:25 +0300

    feat(crypto): add isASCII utility function to validate ASCII strings in certificate processing

--- packages/webcomponents/src/crypto/utils.ts
+++ packages/webcomponents/src/crypto/utils.ts
@@ -11,6 +11,18 @@ import { PemConverter } from './pem_converter';
 
 import { cryptoProvider } from './provider';
 
+const isASCII = (text: string) => {
+  for (let i = 0; i < text.length; i++) {
+    const code = text.charCodeAt(i);
+
+    if (code > 0xFF) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export const certificateRawToBuffer = (raw: string): ArrayBuffer => {
   if (PemConverter.isPem(raw)) {
     return PemConverter.decode(raw)[0];
@@ -26,6 +38,10 @@ export const certificateRawToBuffer = (raw: string): ArrayBuffer => {
 
   if (Convert.isBase64Url(raw)) {
     return Convert.FromBase64Url(raw);
+  }
+
+  if (isASCII(raw)) {
+    return Convert.FromUtf8String(raw, 'ascii');
   }
 
   throw new TypeError("Unsupported format of 'raw' argument. Must be one of DER, PEM, HEX, Base64, or Base4Url");
