f32b2ab1de24f3a133cda0abc838bbfb07dfa9c7
donskov <donskov@users.noreply.github.com>
2026-07-06 15:10:22 +0000

    deploy: e2b2570e6c7aaece881b270a49cd71ca9388da43

---
+++ build/p-C9fCcVVb.js
@@ -0,0 +1,33 @@
+/*!
+ * © Peculiar Ventures https://peculiarventures.com/ - MIT License
+ */
+import{y as t,z as s,B as i,e,F as r,A as n,c as a,G as o,P as h,h as c,j as u,p as f,g,D as l,H as m,N as p}from"./p-DGgAC8AG.js";
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */function d(n){const a=[];const{baseCertificateID:o,entityName:h,objectDigestInfo:c}=n;if(o){const{issuer:r,serial:n,issuerUID:h}=o;const c=[t("Issuer",[...r].map(i)),s("Serial Number",e.Convert.ToHex(n))];if(h&&h.byteLength>0){c.push(s("Issuer UID",e.Convert.ToHex(h)))}a.push(t("Base Certificate ID",c))}if(h){a.push(t("Entity Name",[...h].map(i)))}if(c){const{digestedObjectType:i,digestAlgorithm:n,objectDigest:o,otherObjectTypeID:h}=c;const u=[s("Type",r[i]),s("Algorithm",n.algorithm),s("Digest",e.Convert.ToHex(o))];if(h){u.push(s("Other Type ID",h))}a.push(t("Digest Info",u))}return a}
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */function b(t){var s;if(t.v1Form){return t.v1Form.map(i)}if((s=t.v2Form)===null||s===void 0?void 0:s.issuerName){return[...t.v2Form.issuerName].map(i)}return[]}
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */class A extends n{constructor(t){super(a(t),o);this.thumbprints={};this.type="X.509 Attribute Certificate";this.tag=h.AttributeCertificateTag;const{acinfo:s}=this.asn;this.serialNumber=e.Convert.ToHex(s.serialNumber);this.version=s.version;const i=s.attrCertValidityPeriod.notBeforeTime;if(!i){throw new Error("Cannot get 'notBefore' value")}this.notBefore=i;const r=s.attrCertValidityPeriod.notAfterTime;if(!r){throw new Error("Cannot get 'notAfter' value")}this.notAfter=r;this.validity=c(this.notBefore,this.notAfter);this.issuer=b(s.issuer);this.holder=d(s.holder)}get signature(){const{signatureValue:t,signatureAlgorithm:s}=this.asn;return{value:t,algorithm:s.algorithm}}parseExtensions(){const{acinfo:t}=this.asn;if(t.extensions){this.extensions=t.extensions.map(u)}}parseAttributes(){const{acinfo:t}=this.asn;if(t.attributes){this.attributes=t.attributes.map(f)}}async getThumbprint(t="SHA-1"){try{const s=await g(t,this.raw);if(s){this.thumbprints[t]=e.Convert.ToHex(s)}}catch(t){console.error("Error thumbprint get:",t)}}get commonName(){return`attribute-certificate-${this.thumbprints["SHA-1"]}`}toString(t="pem"){switch(t){case"pem":return h.encode(this.raw,this.tag);case"base64url":return e.Convert.ToBase64Url(this.raw);default:return e.Convert.ToBase64(this.raw)}}downloadAsPEM(t){l.attrCert.asPEM(this.toString("pem"),t||this.commonName)}downloadAsDER(t){l.attrCert.asDER(this.raw,t||this.commonName)}}
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */class D extends n{constructor(t){super(a(t),m);this.thumbprints={};this.type="X.509 Certificate Revocation List";this.tag=h.CrlTag;const{tbsCertList:s}=this.asn;this.issuer=p.parse(s.issuer);this.version=s.version+1;this.lastUpdate=s.thisUpdate.getTime();this.nextUpdate=s.nextUpdate.getTime();this.revokedCertificates=(s.revokedCertificates||[]).map((t=>{var s;return{revocationDate:t.revocationDate,userCertificate:t.userCertificate,crlEntryExtensions:(s=t.crlEntryExtensions)===null||s===void 0?void 0:s.map(u)}}))}async getThumbprint(t="SHA-1"){try{const s=await g(t,this.raw);if(s){this.thumbprints[t]=e.Convert.ToHex(s)}}catch(t){console.error("Error thumbprint get:",t)}}get signature(){const{signature:t,signatureAlgorithm:s}=this.asn;return{value:t,algorithm:s.algorithm}}get commonName(){if(!this.issuer){return""}for(let t=0;t<this.issuer.length;t+=1){const s=this.issuer[t];if(s.short==="CN"||s.short==="E"||s.short==="O"){return s.value}}return""}parseExtensions(){const{tbsCertList:t}=this.asn;if(t.crlExtensions){this.extensions=t.crlExtensions.map(u)}}toString(t="pem"){switch(t){case"pem":return h.encode(this.raw,this.tag);case"base64url":return e.Convert.ToBase64Url(this.raw);default:return e.Convert.ToBase64(this.raw)}}downloadAsPEM(t){l.crl.asPEM(this.toString("pem"),t||this.commonName)}downloadAsDER(t){l.crl.asDER(this.raw,t||this.commonName)}}export{A as X,D as a};
+//# sourceMappingURL=p-C9fCcVVb.js.map
---
+++ build/p-WpaNKxl_.system.js
@@ -0,0 +1,103 @@
+/*!
+ * © Peculiar Ventures https://peculiarventures.com/ - MIT License
+ */
+System.register(["./p-CCyzZ5j5.system.js","./p-CRc9ddUc.system.js","./p-BPj_AI1m.system.js","./p-DfK0DWXj.system.js","./p-at6kSwVv.system.js"],(function(n){"use strict";var e,r,u,t,a,l,i,o,v;return{setters:[function(n){e=n.h},function(n){r=n.x;u=n.v;t=n.w;a=n.e},function(n){l=n.L},function(n){i=n.T;o=n.B},function(n){v=n.D}],execute:function(){n({b:s,g:c,r:L});
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */function c(n,e){if(e===void 0){e=false}var u=r[n];if(e){return u||n}if(u){return"".concat(u," (").concat(n,")")}return n}
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */function s(n){return{getAuthKeyIdParentLink:function(e){var r;return(r=n.authKeyIdParentLink)===null||r===void 0?void 0:r.replace("{{authKeyId}}",e)},getAuthKeyIdSiblingsLink:function(e){var r;return(r=n.authKeyIdSiblingsLink)===null||r===void 0?void 0:r.replace("{{authKeyId}}",e)},getSubjectKeyIdChildrenLink:function(e){var r;return(r=n.subjectKeyIdChildrenLink)===null||r===void 0?void 0:r.replace("{{subjectKeyId}}",e)},getSubjectKeyIdSiblingsLink:function(e){var r;return(r=n.subjectKeyIdSiblingsLink)===null||r===void 0?void 0:r.replace("{{subjectKeyId}}",e)},getIssuerDnLink:function(){return n.issuerDnLink}}}
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */function f(n){return n.indexOf("http")===0}
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var d=n("e",(function(n,r){return e("tr",null,e("td",{colSpan:2},e("table",null,r)))}));var m=n("R",(function(n){var r=n.value;if(!r){return null}return[e("tr",{class:"title"},e("td",{colSpan:2},e(i,{variant:"s1",color:"black"},r))),e("tr",null,e("td",{colSpan:2,class:"divider"},e("span",null)))]}));var p=n("d",(function(n){var r=n.name,u=n.value,t=n.monospace,a=n.collapse,o=n.href,v=n.extraValue;if(!r){return null}if(u===undefined||u===null){return null}var c;if(a){c=e("peculiar-text-hider",null,u)}else{c=u}var s=!!u.toString();return e("tr",null,e("td",{colSpan:s?1:2},e(i,{variant:"b2",color:"gray-9"},r)),s&&e("td",{class:{monospace:t}},f(u.toString())||o?e(l,{variant:"b2",href:o||u.toString()},u):e(i,{variant:"b2",color:"black"},c,v)))}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var h=n("B",(function(n){var r=n.serialNumber,a=n.version,l=n.validity,i=n.notBefore,o=n.notAfter,v=n.lastUpdate,c=n.nextUpdate,s=n.type;return[e(m,{value:u.getString("basicInformation")}),e(p,{name:u.getString("type"),value:s}),e(p,{name:u.getString("serialNumber"),value:r,monospace:true}),e(p,{name:u.getString("version"),value:a}),e(p,{name:u.getString("validity"),value:l}),e(p,{name:u.getString("issued"),value:i?t(i):undefined}),e(p,{name:u.getString("expired"),value:o?t(o):undefined}),e(p,{name:u.getString("lastUpdate"),value:v?t(v):undefined}),e(p,{name:u.getString("nextUpdate"),value:c?t(c):undefined})]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */function y(n){if(n.params&&"modulus"in n.params){var e=n.params.modulus.byteLength;if(e%2){e-=1}return e*8}return null}function b(n){if(n.params&&"publicExponent"in n.params){return n.params.publicExponent.byteLength===3?65537:3}return null}var g=n("P",(function(n){var r=n.publicKey;if(!r){return null}function t(n){return[e(p,{name:u.getString("algorithm"),value:c(n.algorithm)}),e(p,{name:u.getString("namedCurve"),value:c(n.params&&"namedCurve"in n.params?n.params.namedCurve:undefined)}),e(p,{name:u.getString("exponent"),value:b(n)}),e(p,{name:u.getString("modulus"),value:y(n)}),e(p,{name:u.getString("value"),value:a.Convert.ToHex(n.value),monospace:true,collapse:true})]}return[e(m,{value:u.getString("publicKeyInfo")}),t(r),Array.isArray(r.params)&&r.params.length&&r.params.map((function(n){return e(d,null,t(n))}))]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var j=n("S",(function(n){var r=n.name;return[e(m,{value:u.getString("subjectName")}),r.map((function(n){return e(p,{name:c(n.type,true),value:n.value})}))]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var S=n("I",(function(n){var r=n.name,t=n.issuerDnLink;var a=u.getString("issuerName");return[e(m,{value:t?e(l,{href:t},a):a}),r.map((function(n){return e(p,{name:c(n.type,true),value:n.value})}))]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var I=n("a",(function(n){var r=n.signature;if(!r){return null}function t(n){return[e(p,{name:u.getString("algorithm"),value:c(n.algorithm)}),e(p,{name:u.getString("value"),value:a.Convert.ToHex(n.value),monospace:true,collapse:true})]}return[e(m,{value:u.getString("signature")}),t(r),r.params&&r.params.length&&r.params.map((function(n){return e(d,null,t(n))}))]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var K=n("T",(function(n){var r=n.thumbprints;if(!r){return null}var t=Object.keys(r);if(!t.length){return null}return[e(m,{value:u.getString("fingerprints")}),t.map((function(n){return e(p,{name:n,value:r[n],monospace:true})}))]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var k=n("M",(function(n){var r=n.certificate;return[e(m,{value:u.getString("miscellaneous")}),e("tr",null,e("td",null,e(o,{startIcon:e(v,null),onClick:function(){return r.downloadAsPEM()}},u.getString("download.pem")))),e("tr",null,e("td",null,e(o,{startIcon:e(v,null),onClick:function(){return r.downloadAsDER()}},u.getString("download.der"))))]}));
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var x=function(n){return"https://search.gleif.org/#/record/".concat(n)};var C=function(n){return"https://search.censys.io/search?resource=hosts&q=dns.names%3A".concat(n)};var A=function(n){return"https://search.censys.io/search?resource=hosts&q=ip%3A".concat(n)};
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */function N(n){if(!n._type||n.value==null)return undefined;var e=String(n.value);switch(n._type){case"dNSName":return C(e);case"iPAddress":return A(e);case"lei":return x(e);default:return undefined}}function O(n,r){var u,t,a,i;var o=c(n.title,true);var v=N(n);var s=n.value!=null?c(String(n.value)):"";if(n._type==="authorityKeyId"&&n.value!=null){var f=(u=r.getAuthKeyIdParentLink)===null||u===void 0?void 0:u.call(r,s);var d=(t=r.getAuthKeyIdSiblingsLink)===null||t===void 0?void 0:t.call(r,s);return e(p,{name:o,value:s,monospace:true,extraValue:[f&&e("span",null," [",e(l,{href:f},"parents"),"]"),d&&e("span",null," [",e(l,{href:d},"siblings"),"]")]})}if(n._type==="subjectKeyId"&&n.value!=null){var m=(a=r.getSubjectKeyIdChildrenLink)===null||a===void 0?void 0:a.call(r,s);var d=(i=r.getSubjectKeyIdSiblingsLink)===null||i===void 0?void 0:i.call(r,s);return e(p,{name:o,value:s,monospace:true,extraValue:[m&&e("span",null," [",e(l,{href:m},"children"),"]"),d&&e("span",null," [",e(l,{href:d},"siblings"),"]")]})}return e(p,{name:o,value:typeof n.value==="boolean"?n.value?"YES":"NO":s,href:v})}function L(n,r){var u,t,a;if(n.title&&!((u=n.value)===null||u===void 0?void 0:u.toString())){return[e(p,{name:c(n.title),value:""}),e(d,null,(t=n.children)===null||t===void 0?void 0:t.map((function(n){return L(n,r)})))]}if((a=n.children)===null||a===void 0?void 0:a.length){return e(d,null,n.title&&e("tr",null,e("td",{colSpan:2},e(i,{variant:"b2",color:"gray-9"},c(n.title)))),n.children.map((function(n){return L(n,r)})))}return O(n,r)}
+/**
+             * @license
+             * Copyright (c) Peculiar Ventures, LLC.
+             *
+             * This source code is licensed under the MIT license found in the
+             * LICENSE file in the root directory of this source tree.
+             */var w=undefined&&undefined.__rest||function(n,e){var r={};for(var u in n)if(Object.prototype.hasOwnProperty.call(n,u)&&e.indexOf(u)<0)r[u]=n[u];if(n!=null&&typeof Object.getOwnPropertySymbols==="function")for(var t=0,u=Object.getOwnPropertySymbols(n);t<u.length;t++){if(e.indexOf(u[t])<0&&Object.prototype.propertyIsEnumerable.call(n,u[t]))r[u[t]]=n[u[t]]}return r};var E=n("c",(function(n){var r=n.extensions,u=n.title,t=u===void 0?"Extensions":u,a=w(n,["extensions","title"]);if(!(r===null||r===void 0?void 0:r.length)){return null}return[e(m,{value:t}),r.map((function(n){return[e("tr",null,e("td",{colSpan:2},e(i,{variant:"s2",color:"gray-9"},c(n.oid)))),e(p,{name:"Critical",value:n.critical?"YES":"NO"}),n.children.map((function(n){return L(n,a)})),e("tr",null,e("td",{colSpan:2,class:"divider"},e("span",null)))]}))]}))}}}));
+//# sourceMappingURL=p-WpaNKxl_.system.js.map
