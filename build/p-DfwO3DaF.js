8334240f4499735e8f720808cd9f22be569f204d
donskov <donskov@users.noreply.github.com>
2026-07-07 11:12:10 +0000

    deploy: 4e35db55f882ab00ee85e6fc226ac425a017d3d0

---
+++ build/p-DfwO3DaF.js
@@ -0,0 +1,12 @@
+/*!
+ * © Peculiar Ventures https://peculiarventures.com/ - MIT License
+ */
+import{A as t,c as s,C as i,P as e,N as r,i as a,a as h,E as n,b as o,R as c,g as u,e as g,p as l,D as m}from"./p-PBNgg9AF.js";
+/**
+ * @license
+ * Copyright (c) Peculiar Ventures, LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */class f extends t{constructor(t){super(s(t),i);this.thumbprints={};this.type="PKCS#10 Certificate Request";this.tag=e.CertificateRequestTag;const{certificationRequestInfo:a}=this.asn;this.subject=r.parse(a.subject);this.version=a.version}get publicKey(){const{subjectPublicKey:t,algorithm:s}=this.asn.certificationRequestInfo.subjectPKInfo;let i;if(s.algorithm===a&&s.parameters){i=h.parse(s.parameters,n)}if(s.algorithm===o){i=h.parse(t,c)}const e=h.serialize(this.asn.certificationRequestInfo.subjectPKInfo);return{params:i,value:e,algorithm:s.algorithm}}get signature(){const{signature:t,signatureAlgorithm:s}=this.asn;return{value:t,algorithm:s.algorithm}}get commonName(){if(!this.subject){return""}for(let t=0;t<this.subject.length;t+=1){const s=this.subject[t];if(s.short==="CN"||s.short==="E"||s.short==="O"){return s.value}}return""}async getThumbprint(t="SHA-1"){try{const s=await u(t,this.raw);if(s){this.thumbprints[t]=g.Convert.ToHex(s)}}catch(t){console.error("Error thumbprint get:",t)}}parseAttributes(){const{certificationRequestInfo:t}=this.asn;if(t.attributes){this.attributes=t.attributes.map(l)}}toString(t="pem"){switch(t){case"pem":return e.encode(this.raw,this.tag);case"base64url":return g.Convert.ToBase64Url(this.raw);default:return g.Convert.ToBase64(this.raw)}}downloadAsPEM(t){m.csr.asPEM(this.toString("pem"),t||this.commonName)}downloadAsDER(t){m.csr.asDER(this.raw,t||this.commonName)}}export{f as P};
+//# sourceMappingURL=p-DfwO3DaF.js.map
